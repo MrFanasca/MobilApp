e3d83fa23d1001465f810fbc329fb13037a9cc05
Fanasca Fabrizio <fabri.taz5@live.it>
2023-12-05 12:53:48 +0100

    prima stesura

---
+++ src/org/generation/italy/Main.java
@@ -0,0 +1,36 @@
+package org.generation.italy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*Scrivere una classe MobileApp contenente i seguenti attributi:
+		    -nome: stringa
+		    -sistemaOperativo: stringa
+		    -prezzo: float
+		    -totaleValutazioni: intero
+		    -numeroValutazioni: intero
+		    -ricavoTotale: float
+		  e i seguenti metodi:
+		    -MobileApp(String nome, String sistemaOperativo, float prezzo)
+			  (costruttore della classe: inizializza gli attributi nome, sistemaOperativo, prezzo in base ai parametri 
+			  passati; inizializza totaleValutazioni, numeroValutazioni, ricavoTotale a 0)
+		    -download(): incrementa il ricavoTotale del valore del prezzo
+		    -download(int numeroDownload): incrementa il ricavoTotale del valore del prezzo*numeroDownload
+		    -riceviValutazione(int numeroStelle): l'app riceve una valutazione da 1 a 5 stelle, e si aggiornano gli attributi 
+		   	  totaleValutazioni e numeroValutazioni
+		    -float valutazioneMedia(): restituisce la valutazione media ricevuta finora
+		  Nella classe Main:
+			-chiedere all'utente di inserire nome,  sistemaOperativo,  prezzo dell'app
+			-creare un oggetto della classe MobileApp passando al costruttore i parametri inseriti dall'utente
+			-creare un menu con le seguenti voci:
+			 *download singolo
+			 *download multiplo (in questo caso chiedere all'utente il numero di download da effettuare)
+			 *ricevi valutazione (in questo caso chiedere all'utente il numero di stelle da assegnare - da 1 a 5)
+			 *esci
+			-dopo ogni chiamata ad una voce di menu visualizzare i dati della mobile app (valutazione media, ricavo totale)*/
+		
+		
+
+	}
+
+}
---
+++ src/org/generation/italy/MobilApp.java
@@ -0,0 +1,101 @@
+package org.generation.italy;
+
+public class MobilApp {
+
+	private String nome, sistemaOperativo;
+	private float prezzo, ricavoTotale;
+	private int totaleValutazioni, numeroValutazioni;
+    
+    // costruttore
+	public MobilApp(String nome, String sistemaOperativo, float prezzo) {
+
+		this.nome = nome;
+		this.sistemaOperativo = sistemaOperativo;
+		this.prezzo = prezzo;
+		
+		totaleValutazioni=0;
+		numeroValutazioni=0;
+		ricavoTotale=0f;
+	}
+
+	public String getNome() 
+	{
+		return nome;
+	}
+
+	public void setNome(String nome) 
+	{
+		if(!nome.isEmpty())
+			this.nome = nome;
+	}
+
+	public String getSistemaOperativo() 
+	{
+		return sistemaOperativo;
+	}
+
+	public void setSistemaOperativo(String sistemaOperativo) 
+	{
+		if(!nome.isEmpty())
+			this.sistemaOperativo = sistemaOperativo;
+	}
+
+	public float getPrezzo() 
+	{
+		return prezzo;
+	}
+
+	public boolean setPrezzo(float prezzo) 
+	{
+		boolean esito = false;
+		if (prezzo>0)
+		{
+			this.prezzo = prezzo;
+			esito=true;
+		}
+		return esito;
+	}
+
+	// metodo che permette di incrementare il ricavo complessivo dopo un singolo download
+	public void downloadSingolo ()
+	{
+		this.ricavoTotale+=prezzo;
+	}
+	
+	// metodo che permette di incrementare il ricavo complessivo dopo un download multiplo
+	public boolean downloadMultilpo (int numeroDownload)
+	{
+		boolean esito = false;
+		if (numeroDownload>0)
+		{
+			this.ricavoTotale+=prezzo*numeroDownload;
+			esito = true;
+		}
+		
+		return esito;	
+	}
+	
+	public float getRicavoTot ()
+	{
+		return ricavoTotale;
+	}
+	
+	// metodo per la valutazione dell'applicazione
+	public boolean riceviValutazione (int numeroStelle)
+	{
+		boolean esito = false;
+		if (numeroStelle>1 && numeroStelle<5)
+		{
+			this.totaleValutazioni+=numeroStelle;
+			this.numeroValutazioni+=1;
+			esito=true;
+		}
+		return esito;
+	}
+    
+	// restituisce la valutazione media ricevuta finora
+	public void valutazioneMedia ()
+	{
+		float valutazioneMedia = (float) totaleValutazioni/numeroValutazioni;
+	}
+}
